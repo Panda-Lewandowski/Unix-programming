190c3f2e91f694c664a2707d0e7a8bfd6c48b3fb
Pandas <daria-shcherbatyuk@yandex.ru>
2017-11-15 17:35:08 +0300

    Update task2.c

--- LinuxLab2/task2.c
+++ LinuxLab2/task2.c
@@ -1,8 +1,8 @@
 /*
-* íàïèñàòü ïðîãðàììó ïî ñõåìå ïåðâîãî çàäàíèÿ, íî â ïðîöåññå-ïðåäêå 
-* âûïîëíèòü ñèñòåìíûé âûçîâ wait(). Óáåäèòüñÿ, ÷òî â ýòîì ñëó÷àå 
-* èäåíòèôèêàòîð ïðîöåññà ïîòîìêà íà 1 áîëüøå èäåíòèôèêàòîðà 
-* ïðîöåññà-ïðåäêà.
+* написать программу по схеме первого задания, но в процессе-предке 
+* выполнить системный вызов wait(). Убедиться, что в этом случае 
+* идентификатор процесса потомка на 1 больше идентификатора 
+* процесса-предка.
 */
 
 #include <stdio.h> //printf
@@ -21,25 +21,25 @@ int main()
 	}
 	if ( child == 0 )
 	{
-        //ïîòîìñòâåííûé êîä
-        printf( "Child: pid=%d;	group=%d;	parent=%d\n", getpid(), getpgrp(), getppid() );
-        int ret_value = getpid() % 2;
-        printf( "Child: returning %d.\n", ret_value);
-        return ret_value;
+		//потомственный код
+		printf( "Child: pid=%d;	group=%d;	parent=%d\n", getpid(), getpgrp(), getppid() );
+		int ret_value = getpid() % 2;
+		printf( "Child: returning %d.\n", ret_value);
+		return ret_value;
 	}
 	else
 	{
-		//ðîäèòåëüñêèé êîä
-        printf( "Parent: pid=%d;	group=%d;	child=%d\n", getpid(), getpgrp(), child );
-        int status;
-        pid_t ret_value;
-        ret_value = wait( &status );
-        if ( WIFEXITED(status) )
-            printf("Parent: child %d finished with %d code.\n", ret_value, WEXITSTATUS(status) );
-        else if ( WIFSIGNALED(status) )
-            printf( "Parent: child %d finished from signal with %d code.\n", ret_value, WTERMSIG(status));
-        else if ( WIFSTOPPED(status) )
-            printf("Parent: child %d finished from signal with %d code.\n", ret_value, WSTOPSIG(status));
+		//родительский код
+		printf( "Parent: pid=%d;	group=%d;	child=%d\n", getpid(), getpgrp(), child );
+		int status;
+		pid_t ret_value;
+		ret_value = wait( &status );
+		if ( WIFEXITED(status) )
+		    printf("Parent: child %d finished with %d code.\n", ret_value, WEXITSTATUS(status) );
+		else if ( WIFSIGNALED(status) )
+		    printf( "Parent: child %d finished from signal with %d code.\n", ret_value, WTERMSIG(status));
+		else if ( WIFSTOPPED(status) )
+		    printf("Parent: child %d finished from signal with %d code.\n", ret_value, WSTOPSIG(status));
 		return 0;
 	}
 }
